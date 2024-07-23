88b46ee0ca7710bacb1ad3577c3f54aa29906258
Kateryna-Katya <katyaukr2014@gmail.com>
2024-07-23 14:59:24 +0300

    add

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,37 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const input = document.querySelector('#controls input');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const boxesContainer = document.getElementById('boxes');
+
+createBtn.addEventListener('click', () => {
+  const amount = parseInt(input.value, 10);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = '';
+  }
+});
+
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  boxesContainer.innerHTML = '';
+
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement('div');
+    const size = 30 + i * 10;
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    fragment.appendChild(div);
+  }
+
+  boxesContainer.appendChild(fragment);
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
